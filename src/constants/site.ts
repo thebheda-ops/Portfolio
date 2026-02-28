10ef67bad3d9f711f30e7bfa29a01ffb986af406
thebheda-ops <thederay802@gmail.com>
2026-02-28 20:18:07 +0545

    brand and linkedin and resume link added

--- src/constants/site.ts
+++ src/constants/site.ts
@@ -1,11 +1,12 @@
 export const siteConfig = {
-  name: "Bal Krishna",
-  brand: "BK",
-  email: "hello@example.com",
+  name: "Bal Krishna Thapa Magar",
+  brand: "BTM",
+  email: "balkrishnathapamagar2005@gmail.com",
   location: "Nepal",
   social: {
     github: "https://github.com/theBheda-ops",
-    linkedin: "https://linkedin.com/in/your-username",
+    linkedin: "https://www.linkedin.com/in/bal-krishna-thapa-magar-02603a346/",
   },
-  resumeUrl: "#",
+  resumeUrl:
+    "https://drive.google.com/file/d/1d7vur4XYR1GZV_8hjLfdU52UXKO7q8Z1/view?usp=drive_link",
 };
