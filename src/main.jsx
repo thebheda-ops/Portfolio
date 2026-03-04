efeb60fb287b6b945c78d7722d5ef2deb3008045
thebheda-ops <thederay802@gmail.com>
2026-03-04 13:59:40 +0545

    maintenance mode

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "preact/compat/client";
 import App from "./app/App";
 import "./styles/global.css";
 
-const SITE_IN_MAINTENANCE = false;
+const SITE_IN_MAINTENANCE = true;
 
 function MaintenanceScreen() {
   return (
